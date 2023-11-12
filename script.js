d9f95c38b4858cc8956d63929c84e0b3b9f342d4
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-12 12:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function calculateGrade(percentage) {
   //your code here
+	if (percentage >= 90 && percentage <= 100) {
+    return "A";
+  } else if (percentage >= 80 && percentage <= 89) {
+    return "B";
+  } else if (percentage >= 70 && percentage <= 79) {
+    return "C";
+  } else if (percentage >= 60 && percentage <= 69) {
+    return "D";
+  } else {
+    return "F";
+  }
 }
+
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
